0279e9ed8cdae0714fb1dc91c625ec91cbb5c286
Devika V <devika1997eee@gmail.com>
2022-02-01 07:36:16 +0000

    Blog-RouterPractice

---
+++ src/components/NotFound/index.js
@@ -0,0 +1,14 @@
+import './index.css'
+
+const NotFound = () => (
+  <div className="not-found-container">
+    <h1 className="notfnd-head">404</h1>
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/not-found-blog-img.png"
+      alt="not-found"
+      className="not-found-img"
+    />
+  </div>
+)
+
+export default NotFound
